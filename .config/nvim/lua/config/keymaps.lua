c369f65ec2505fce6fe59f8167738e321e406ac0
ennanco <ennanco@gmail.com>
2026-04-21 18:20:12 +0200

    Add shared movement and clipboard keymaps for Vim and Neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,16 @@ map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+map("n", "J", "mzJ`z", { desc = "Join lines and keep cursor" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Half-page down centered" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half-page up centered" })
+map("n", "n", "nzzzv", { desc = "Next search centered" })
+map("n", "N", "Nzzzv", { desc = "Previous search centered" })
+
+map("x", "<leader>p", [=["_dP]=], { desc = "Paste without yanking" })
+map({ "n", "v" }, "<leader>y", [=["+y]=], { desc = "Yank to system clipboard" })
+map("n", "<leader>Y", [=["+Y]=], { desc = "Yank line to system clipboard" })
