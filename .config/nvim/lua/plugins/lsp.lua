d9c310681a98b62b010e4e88bdd32250db892b66
ennanco <ennanco@gmail.com>
2026-04-21 11:15:45 +0200

    Fix Neovim LSP startup and add mini modules

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,24 +26,30 @@ return {
     },
     config = function(_, opts)
       local lspconfig = require("lspconfig")
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local has_blink, blink = pcall(require, "blink.cmp")
+      if has_blink then
+        capabilities = blink.get_lsp_capabilities(capabilities)
+      end
 
       require("mason-lspconfig").setup(opts)
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
+
+      for _, server_name in ipairs(opts.ensure_installed or {}) do
+        if server_name ~= "omnisharp" and lspconfig[server_name] then
           lspconfig[server_name].setup({
             capabilities = capabilities,
           })
-        end,
-        ["omnisharp"] = function()
-          lspconfig.omnisharp.setup({
-            capabilities = capabilities,
-            enable_editorconfig_support = true,
-            enable_import_completion = true,
-            analyze_open_documents_only = false,
-          })
-        end,
-      })
+        end
+      end
+
+      if lspconfig.omnisharp then
+        lspconfig.omnisharp.setup({
+          capabilities = capabilities,
+          enable_editorconfig_support = true,
+          enable_import_completion = true,
+          analyze_open_documents_only = false,
+        })
+      end
     end,
   },
 }
---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    opts = {
+      n_lines = 500,
+    },
+  },
+  {
+    "echasnovski/mini.icons",
+    event = "VeryLazy",
+    opts = {},
+  },
+}
